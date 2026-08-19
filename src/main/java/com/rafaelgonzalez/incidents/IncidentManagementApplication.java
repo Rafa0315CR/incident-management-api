1b089d5bac86e29fe875fac88a8a00670750bc33
Rafa0315CR <108423434+Rafa0315CR@users.noreply.github.com>
2026-08-18 23:00:50 -0600

    Agregar src/main/java/com/rafaelgonzalez/incidents/IncidentManagementApplication.java

---
+++ src/main/java/com/rafaelgonzalez/incidents/IncidentManagementApplication.java
@@ -0,0 +1,12 @@
+package com.rafaelgonzalez.incidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentManagementApplication.class, args);
+    }
+}
